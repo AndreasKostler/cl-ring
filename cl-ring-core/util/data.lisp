c2199784ff8fa465839e17e27aa22b7ff53f2d31
Andreas Koestler <andreas.koestler@gmail.com>
2012-09-06 16:13:54 +0930

    Fixup some doc strings.

--- cl-ring-core/util/data.lisp
+++ cl-ring-core/util/data.lisp
@@ -1,21 +1,21 @@
 (in-package #:cl-ring-util)
 
 (defun getval (alist key)
-  "Returns the va(lue for key"
+  "Returns the value for key."
   (cdr (assoc key alist)))
 
 (defun remval (alist key)
+  "Removes key from alist."
   (remove key alist :key #'car))
 
 (defun putval (alist key val)
-  "Returns a new map that contains the mapping of key(s) to val(s)."
+  "Returns a new map that contains the mapping of key to val."
   (acons key val (remval alist key)))
   
  
 (defun putval-in (alist keys val)
-  "Associates a value in a nested associative structure, where ks is a
-sequence of keys and v is the new value and returns a new nested structure.
-If any levels do not exist, hash-maps will be created."
+  "Associates a value in a nested alist, where keys is a sequence of keys
+ and val is the new value and returns a new nested alist. If any levels do not exist, alists will be created."
   (destructuring-bind (key &rest keys) keys
     (if keys
 	(putval alist key (putval-in (getval alist key) keys val))
@@ -23,7 +23,7 @@ If any levels do not exist, hash-maps will be created."
 
 (defun putval-cons (alist key val)
   "Associate a key with a value in a map. If the key already exists in the map,
-  a vector of values is associated with the key."
+  a list of values is associated with the key."
   (putval alist key
 	     (let ((cur (getval alist key)))
 	       (if cur
