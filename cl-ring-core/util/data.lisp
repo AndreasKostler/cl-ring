accaedf66c0ba4a5be286324f6c01600097305af
Andreas Koestler <andreas.koestler@gmail.com>
2012-09-06 22:48:47 +1000

    Compare with equal to support string keys

--- cl-ring-core/util/data.lisp
+++ cl-ring-core/util/data.lisp
@@ -1,32 +1,46 @@
 (in-package #:cl-ring-util)
+(annot:enable-annot-syntax)
 
-(defun getval (alist key)
+@export
+(defun getval (m k)
   "Returns the value for key."
-  (cdr (assoc key alist)))
+  (cdr (assoc k m :test #'equal)))
 
-(defun remval (alist key)
+@export
+(defun remval (m k)
   "Removes key from alist."
-  (remove key alist :key #'car))
+  (remove k m :key #'car :test #'equal))
 
-(defun putval (alist key val)
+@export
+(defun putval (m k v)
   "Returns a new map that contains the mapping of key to val."
-  (acons key val (remval alist key)))
+  (acons k v (remval m k)))
   
- 
-(defun putval-in (alist keys val)
+@export
+(defun putval-in (m ks v)
   "Associates a value in a nested alist, where keys is a sequence of keys
  and val is the new value and returns a new nested alist. If any levels do not exist, alists will be created."
-  (destructuring-bind (key &rest keys) keys
-    (if keys
-	(putval alist key (putval-in (getval alist key) keys val))
-	(putval alist key val))))
+  (destructuring-bind (k &rest ks) ks
+    (if ks
+	(putval m k (putval-in (getval m k) ks v))
+	(putval m k v))))
 
-(defun putval-cons (alist key val)
+@export
+(defun putval-cons (m k v)
   "Associate a key with a value in a map. If the key already exists in the map,
   a list of values is associated with the key."
-  (putval alist key
-	     (let ((cur (getval alist key)))
-	       (if cur
-		   (cons val (if (consp cur) cur (list cur)))
-		   val))))
+  (putval m k
+	  (let ((cur (getval m k)))
+	    (if cur
+		(cons v (if (consp cur) cur (list cur)))
+		v))))
+ 
+(defun merge-ms* (m1 m2)
+  (loop for (k . v) in m1
+     unless (getval m2 k)
+     collecting `(,k . ,v) into l
+     finally (return (concatenate 'list l m2))))
 
+@export
+(defun merge-ms (&rest ms)
+  (reduce #'merge-ms* (rest ms) :initial-value (first ms)))
